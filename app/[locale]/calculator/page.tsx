ab18ae249c052a37d4122806d8e59b0a4763dc93
korcevoy1994 <korcevoy.ui@gmail.com>
2026-02-24 13:46:46 +0200

    fix

--- app/[locale]/calculator/page.tsx
+++ app/[locale]/calculator/page.tsx
@@ -2,6 +2,8 @@ import { unstable_cache } from "next/cache";
 import { getCalculatorData } from "@/lib/calculator-queries";
 import { CalculatorClient } from "./calculator-client";
 
+export const dynamic = "force-dynamic";
+
 const getCachedData = unstable_cache(getCalculatorData, ["calc-config"], {
   tags: ["calc-config"],
 });
